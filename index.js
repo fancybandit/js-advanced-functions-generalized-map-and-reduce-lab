b8e494452d5231bebb92d022c5c52178a95fcdd3
Jacob Klimenko <droidedjake@gmail.com>
2021-07-25 20:00:20 -0400

    commit tree 58c932961f66ea0be3044baf700676cfddd9ad35

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(sourceArray, cbFunc) {
+    const newArray = [];
+    for (const val of sourceArray) {
+        newArray.push(cbFunc(val));
+    };
+    return newArray;
+};
+
+function reduce(sourceArray, cbFunc, startingPoint=0) {
+    let current = startingPoint;
+
+    for (const val of sourceArray) {
+        if (val === sourceArray[0] && !current) {
+            current = val;
+        } else {
+            current = cbFunc(val, current);
+        };
+    };
+    
+    return current;
+};
